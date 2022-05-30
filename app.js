d2945112f41fa187a0a88c0c923ef7963498cbe4
Gourab Raj <gourav.raj42001@gmail.com>
2022-05-30 19:09:15 +0530

    Router in Express and Middleware

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require("dotenv").config(); //reasons for saying this file bcoz .env file dont
-                            //get uploaded on github pages
+//get uploaded on github pages
 
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+const authRoutes = require("./routes/auth.js");
+
+//DB Connection
 mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true, //compulsoryyyyyy
@@ -15,9 +21,19 @@ mongoose
     console.log("DB Connected ... ");
   });
 
+//Middlewares
+app.use(bodyParser.json()); //These all are Middleware
+app.use(cookieParser());
+app.use(cors());
+
+//My Routes
+app.use("/api", authRoutes);
+
+//My PORT
 // const port = 8000;
 const port = process.env.PORT || 3000; //Standard Way
 
+//Starting a Server
 app.listen(port, () => {
   console.log(`App is running at ${port}`);
 });
