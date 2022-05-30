9c6ee352c4e540884ba1850451fcd86f1636e5f1
Gourab Raj <gourav.raj42001@gmail.com>
2022-05-30 22:26:15 +0530

    Validation Added .....

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const { check, validationResult } = require("express-validator");
 
 // exports.signup = (req, res) => {
 //   console.log("REQ BODY", req.body);
@@ -8,11 +9,17 @@ const User = require("../models/user");
 // };
 
 exports.signup = (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
   const user = new User(req.body);
   user.save((err, user) => {
     if (err) {
       return res.status(400).json({
-        err: "NOT able to save USer in DB",
+        err: "Not able to save in DB", //It will come if required fields are not Given
       });
     }
     // res.json(user);//Iw will Show all the Fields ...
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var router = express.Router();
+const { check, validationResult } = require("express-validator");
 const { signout, signup } = require("../controllers/auth.js");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [
+    check("name", "Name Should be at least 3 charecters").isLength({ min: 5 }),
+    check("email", "email is Required").isEmail(),
+    check("password", "Password Should be atleast 3 char").isLength({ min: 3 }),
+  ],
+  signup
+);
 router.get("/signout", signout); //get:- Getting from Database ,post:- Putting to DB ,delete:- Deleting ,put:- Update in DB
 
 module.exports = router;
